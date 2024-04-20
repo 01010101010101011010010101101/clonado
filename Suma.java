346d388619ae728b3ba24a9ba9c0f6cb4f723250
Daniel Sanchez <166523536+01010101010101011010010101101@users.noreply.github.com>
2024-04-20 15:13:41 -0500

    actualizacion Suma.java
    modificación del setter and getter de suma

--- Suma.java
+++ Suma.java
@@ -1,6 +1,24 @@
+public class Suma {
+    int n1, n2, result;
+    
+    Suma() {}
+
+    public void setSuma(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+
+        this.result = this.n1 * this.n2; 
+    }
+
+    public int getSuma() {
+        return result;
+    }
+}
+
+/*
 public class Suma{
 	int n1, n2, result;
-	//constructor 	
+	 	
 	Suma(){}
 
 	public void  setSuma(int n1 , int n2){
@@ -17,3 +35,6 @@ public class Suma{
 	
 
 }
+
+
+} */
